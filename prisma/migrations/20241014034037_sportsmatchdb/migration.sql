fbd7bb3455b13cf8bdb1f16c406e6c2a0e1a0355
vitor hugo de abreu schell <vitorlegalhugi@gmail.com>
2024-10-14 01:29:01 -0300

    Teste Depoly para o Railway, adicionando script de start

--- prisma/migrations/20241014034037_sportsmatchdb/migration.sql
+++ prisma/migrations/20241014034037_sportsmatchdb/migration.sql
@@ -1,11 +1,45 @@
--- DropIndex
-DROP INDEX `Esporte_usuarioId_fkey` ON `esporte`;
+-- CreateTable
+CREATE TABLE `Usuario` (
+    `idusuario` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `cidade` VARCHAR(191) NULL,
+    `latitude` DOUBLE NULL,
+    `longitude` DOUBLE NULL,
+    `perfilFoto` VARCHAR(191) NULL,
+    `biografia` VARCHAR(500) NULL,
+    `senha` VARCHAR(191) NOT NULL,
+    `datanasc` DATETIME(3) NOT NULL,
+    `genero` ENUM('fem', 'masc', 'outro') NOT NULL DEFAULT 'outro',
+    `online` DATETIME(3) NOT NULL,
+    `nick` VARCHAR(191) NULL,
 
--- DropIndex
-DROP INDEX `MensagemInd_destinatarioId_fkey` ON `mensagemind`;
+    UNIQUE INDEX `Usuario_idusuario_key`(`idusuario`),
+    UNIQUE INDEX `Usuario_nome_key`(`nome`),
+    UNIQUE INDEX `Usuario_email_key`(`email`),
+    PRIMARY KEY (`idusuario`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- DropIndex
-DROP INDEX `MensagemInd_remetenteId_fkey` ON `mensagemind`;
+-- CreateTable
+CREATE TABLE `Esporte` (
+    `idesporte` INTEGER NOT NULL AUTO_INCREMENT,
+    `usuarioId` INTEGER NOT NULL,
+    `nome` VARCHAR(500) NOT NULL,
+
+    PRIMARY KEY (`idesporte`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `MensagemInd` (
+    `idmensagem` INTEGER NOT NULL AUTO_INCREMENT,
+    `remetenteId` INTEGER NOT NULL,
+    `destinatarioId` INTEGER NOT NULL,
+    `texto` VARCHAR(1000) NOT NULL,
+    `horario` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `MensagemInd_idmensagem_key`(`idmensagem`),
+    PRIMARY KEY (`idmensagem`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Grupo` (
@@ -22,13 +56,11 @@ CREATE TABLE `Grupo` (
 CREATE TABLE `Grupo_Usuario` (
     `idGU` INTEGER NOT NULL AUTO_INCREMENT,
     `adm` BOOLEAN NOT NULL,
+    `pedido` BOOLEAN NOT NULL,
     `usuarioId` INTEGER NOT NULL,
     `grupoId` INTEGER NOT NULL,
 
     UNIQUE INDEX `Grupo_Usuario_idGU_key`(`idGU`),
-    INDEX `Grupo_Usuario_usuarioId_idx`(`usuarioId`),
-    INDEX `Grupo_Usuario_grupoId_idx`(`grupoId`),
-    UNIQUE INDEX `Grupo_Usuario_usuarioId_grupoId_key`(`usuarioId`, `grupoId`),
     PRIMARY KEY (`idGU`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
